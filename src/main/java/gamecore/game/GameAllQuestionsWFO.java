3ee4252794278bdbd2a62e3791f1536b3bc1670c
Haseoo <dawid_szymkiewicz@hotmail.com>
2019-09-07 23:04:26 +0200

    Add without falling out mode

--- src/main/java/gamecore/game/GameAllQuestionsWFO.java
+++ src/main/java/gamecore/game/GameAllQuestionsWFO.java
@@ -1,6 +1,7 @@
 package gamecore.game;
 
 import exceptions.repositories.RepositoryInitalizationException;
+import gamecore.game.enums.FallingOutPolicy;
 import gamecore.projections.players.Player.PlayerInfo;
 import gamecore.repositories.QuestionRepository;
 import gamecore.services.IPlayerService;
@@ -10,18 +11,35 @@ import gamecore.services.QuestionService;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
+import static gamecore.game.enums.FallingOutPolicy.WITHOUT_PLAYERS_FALLING_OUT;
+import static gamecore.game.enums.FallingOutPolicy.WITH_PLAYERS_FALLING_OUT;
 import static gamecore.utility.Constants.DEFAULT_SCORE_INCREMENT_VALUE;
 
 @Slf4j
 public class GameAllQuestionsWFO extends AbstractGame {
 
+    private static final Map<FallingOutPolicy, BiConsumer<IPlayerService, PlayerInfo>> fallingOutAction;
+
+    static {
+        fallingOutAction = new HashMap<>();
+        fallingOutAction.put(WITH_PLAYERS_FALLING_OUT, (IPlayerService::setPlayerLost));
+        fallingOutAction.put(WITHOUT_PLAYERS_FALLING_OUT, (s, p) -> {});
+    }
+
+    private FallingOutPolicy fallingOutPolicy;
+
     public GameAllQuestionsWFO(IQuestionService questionService,
-                               IPlayerService playerService) {
+                               IPlayerService playerService,
+                               FallingOutPolicy fallingOutPolicy) {
         super(questionService,
                 playerService,
                 questionService.getNumberOfAvailableQuestions());
+        this.fallingOutPolicy = fallingOutPolicy;
     }
 
     @Override
@@ -30,7 +48,7 @@ public class GameAllQuestionsWFO extends AbstractGame {
         if (isCorrectAnswer) {
             getPlayerService().addPointsToPlayerScore(answeringPlayer, DEFAULT_SCORE_INCREMENT_VALUE);
         } else {
-            getPlayerService().setPlayerLost(answeringPlayer);
+            fallingOutAction.get(fallingOutPolicy).accept(getPlayerService(), answeringPlayer);
         }
         return isCorrectAnswer;
     }
@@ -38,7 +56,8 @@ public class GameAllQuestionsWFO extends AbstractGame {
     public static void main(String...args) throws RepositoryInitalizationException {
         List<String> testPlayers = Arrays.asList("Anna", "Wanna");
         IGame game = new GameAllQuestionsWFO(new QuestionService(new QuestionRepository()),
-                                         new PlayerService(testPlayers));
+                                             new PlayerService(testPlayers),
+                                             WITH_PLAYERS_FALLING_OUT);
         PlayerInfo[] players = game.getPlayers();
         while (!game.isGameEnded()) {
             log.info("---------");
